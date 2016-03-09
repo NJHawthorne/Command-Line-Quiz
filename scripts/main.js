a3d9ed4f15e64d735548994c384483ec1b23639d
Nate Hawthorne <natejhawthorne@gmail.com>
2016-03-08 23:43:08 -0600

    Just gonna drop the completed files in. Whoop whoop.

---
+++ scripts/main.js
@@ -0,0 +1,172 @@
+var totalAnswers = 15;
+var correctAnswers = 0;
+
+console.log("#1: In Javascript, you can use Math.floor to round decimal numbers to the next highest integer.");
+var answer1 = prompt("Please type in either 'true' or 'false'.");
+if(answer1 === 'false' || answer1 === 'False' || answer1 === 'FALSE') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was false.");
+}
+console.log("#2: To connect your CSS file to your HTML file, you use the <link> tag.");
+var answer2 = prompt("Please type in either 'true' or 'false'.");
+if(answer2 === 'true' || answer2 === 'True' || answer2 === 'TRUE') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was true.");
+}
+console.log("#3: In CSS, an ID outranks a Class in order of importance/specificity.");
+var answer3 = prompt("Please type in either 'true' or 'false'.");
+if(answer3 === 'true' || answer3 === 'True' || answer3 === 'TRUE') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was true.");
+}
+console.log("#4: In Javascript, == and === mean the exact same thing.");
+var answer4 = prompt("Please type in either 'true' or 'false'.");
+if(answer4 === 'false' || answer4 === 'False' || answer4 === 'FALSE') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was false.");
+}
+console.log("#5: In HTML, the <div> tag is an example on an inline-block element.");
+var answer5 = prompt("Please type in either 'true' or 'false'.");
+if(answer5 === 'false' || answer5 === 'False' || answer5 === 'FALSE') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was false.");
+}
+console.log("#6: Which of the following is an example of an inline element in HTML?");
+console.log("A. <nav>");
+console.log("B. <footer>");
+console.log("C. <span>");
+var answer6 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer6 === 'c' || answer6 === 'C') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'C'.");
+}
+console.log("Which of the following would you use to center content in CSS?");
+console.log("A. padding: 0 auto;");
+console.log("B. margin: 0 auto;");
+console.log("C. float: center;");
+var answer7 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer7 === 'b' || answer7 === 'B') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'B'.");
+}
+console.log("What is the proper syntax for a media query in CSS?");
+console.log("A. $media ()");
+console.log("B. &media ()");
+console.log("C. @media ()");
+var answer8 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer8 === 'c' || answer8 === 'C') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'C'.");
+}
+console.log("In Javascript, how do you determine how many characters are in a string?");
+console.log("A. string.length");
+console.log("B. string.chars");
+console.log("C. string.letters");
+var answer9 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer9 === 'a' || answer9 === 'A') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'A'.");
+}
+console.log("What does the % operator do in Javascript?");
+console.log("A. returns the percentage value between 2 numbers");
+console.log("B. returns the remainder when 2 numbers are divided");
+console.log("C. breaks javascript altogether forever. FOR THE LOVE OF GOD DON'T USE %.");
+var answer10 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer10 === 'b' || answer10 === 'B') {
+	correctAnswers++;
+	console.log("It would be more hilarious if the real answer was 'C', but alas, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'B'.");
+}
+console.log("What does HTML stand for?");
+console.log("A. How To Make Lunch");
+console.log("B. Hyperlink Text-based Marker Language");
+console.log("C. HyperText Markup Language");
+var answer11 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer11 === 'c' || answer11 === 'C') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'C'.");
+}
+console.log("In CSS, how do you apply opacity?");
+console.log("A. background-color: rgba();");
+console.log("B. background-color: #$$$$$$.%;");
+console.log("C. background-opacity: %;");
+var answer12 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer12 === 'a' || answer12 === 'A') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'A'.");
+}
+console.log("In Javascript, what would happen if you created a while loop with a condition that never changed?");
+console.log("A. The loop would eventually cancel out and pop up with an error.");
+console.log("B. Your internet browser (Chrome) would crash and you'd have to kill the page.");
+console.log("C. Your laptop would catch on fire.");
+var answer13 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer13 === 'b' || answer13 === 'B') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'B'.");
+}
+console.log("Which of the following would send an error in Javascript?");
+console.log("A. var age;");
+console.log("B. age = 25;");
+console.log("C. age;");
+var answer14 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer14 === 'c' || answer14 === 'C') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'C'.");
+}
+console.log("In the terminal (Yeah that's right, breaking all the rules now), what is the order you need to type commands in to post your content to github?");
+console.log("A. git add, git commit, git push");
+console.log("B. git commit, git add, git push");
+console.log("C. git add, git push, git commit");
+var answer15 = prompt("Please type either 'A', 'B', or 'C' for your answer.");
+if(answer15 === 'a' || answer15 === 'A') {
+	correctAnswers++;
+	console.log("Good job, you got the right answer!");
+}
+else {
+	console.log("Not quite, the correct answer was 'A'.");
+}
+
+console.log("Congratulations, you've reached the end of the quiz. You've survived 15 pop-up questions and you didn't fling the laptop away in rage. I'm proud of you.");
+console.log("You got "+correctAnswers+" answers correct. Noice!");
+console.log("You scored "+(correctAnswers/totalAnswers*100)+"%.");
